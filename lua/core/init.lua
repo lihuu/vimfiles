8daae2b8b501c41b8dff0d4bbcc6d54a0551df17
lihuu <lihu1449488533@outlook.com>
2021-11-08 15:29:27 +0800

    nvim-tree config

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -74,7 +74,39 @@ end
 
 local lihuu_nvim_tree_config = function ()
     vim.g.nvim_tree_gitignore=1
+    --"0 by default, closes the tree when you open a file
+    vim.g.nvim_tree_quit_on_open = 1 
+    --"0 by default, this option shows indent markers when folders are open
+    vim.g.nvim_tree_indent_markers = 1 
+    --"0 by default, will enable file highlight for git attributes (can be used without the icons).
+    vim.g.nvim_tree_git_hl = 1 
+    --"0 by default, will enable folder and file icon highlight for opened files/directories.
+    vim.g.nvim_tree_highlight_opened_files = 1 
+    --"This is the default. See :help filename-modifiers for more options
+    vim.g.nvim_tree_root_folder_modifier = ':~' 
+    --"0 by default, append a trailing slash to folder names
+    vim.g.nvim_tree_add_trailing = 1 
+    --" 0 by default, compact folders that only contain a single folder into one node in the file tree
+    vim.g.nvim_tree_group_empty = 1 
+    --"0 by default, will disable the window picker.
+    vim.g.nvim_tree_disable_window_picker = 1 
+    --"one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
+    vim.g.nvim_tree_icon_padding = ' ' 
+    --"0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
+    vim.g.nvim_tree_respect_buf_cwd = 1 
+    --"1 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
+    vim.g.nvim_tree_create_in_closed_folder = 0 
+    --"1000 by default, control how often the tree can be refreshed, 1000 means the tree can be refresh once per 1000ms.
+    vim.g.nvim_tree_refresh_wait = 500 
+    vim.g.nvim_tree_window_picker_exclude = { filetype= { 'notify', 'packer', 'qf' }, buftype= { 'terminal' } }
+--" Dictionary of buffer option names mapped to a list of option values that
+---" indicates to the window picker that the buffer's window should not be
+--" selectable.
+    --" List of filenames that gets highlighted with NvimTreeSpecialFile
+    vim.g.nvim_tree_special_files = { README= 1, Makefile= 1, MAKEFILE= 1 } 
+    vim.g.nvim_tree_show_icons = { git= 1, folders= 1, files= 1, folder_arrows= 1 }
 end
+
 
 local load_core = function()
     if vim.g.vscode then
@@ -82,6 +114,7 @@ local load_core = function()
     end
 
     lihuu_statify_config()
+    --lihuu_nvim_tree_config()
     lihuu_sqlite_config()
     createdir()
     disable_distribution_plugins()
