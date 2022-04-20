ed9e37e546998af01ed9a34a2388e31dc439bbeb
SilentStorm <1449488533qq@gmail.com>
2022-04-20 23:28:50 +0800

    fix format lua

--- lua/plugins/luaformatter/init.lua
+++ lua/plugins/luaformatter/init.lua
@@ -7,7 +7,7 @@ end
 local sub_array = function(source_array, start_index, end_index)
   local resultArray = {}
   for i = start_index, end_index, 1 do
-    resultArray[i] = source_array[i]
+    table.insert(resultArray, source_array[i])
   end
   return resultArray
 end
@@ -16,14 +16,20 @@ local _M = {}
 
 function _M.LuaFormat()
   local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-  -- local config_file = vim.fn.findfile('.lua-format','.;')
-  -- local options
-  -- if config_file == '' then
-  -- options = ""
-  -- else
-  -- options = " -c " .. config_file
-  -- end
-  local outout_str = vim.fn.system('lua-format -i', lines)
+  local input = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+
+  local error_file = vim.fn.tempname()
+
+  local config_file = vim.fn.findfile(".lua-format", ",;")
+
+  local flags = " -i "
+
+  if not config_file then
+    flags = flags .. " -c " .. config_file
+  end
+
+  local outout_str = vim.fn.system(
+                       'lua-format' .. flags .. " 2> " .. error_file, input)
 
   if #outout_str > 0 then
     local output = vim.split(outout_str, "\n")
@@ -31,7 +37,7 @@ function _M.LuaFormat()
     -- alert(min_line)
     for i = 1, min_line, 1 do
       local output_line = output[i]
-      local input_line = lines[i]
+      local input_line = input[i]
       if not (output_line == input_line) then
         vim.fn.setline(i, output_line)
       end
@@ -42,13 +48,18 @@ function _M.LuaFormat()
         vim.fn.deletebufline(vim.fn.bufname('%'), min_line + 1, "$")
       else
         local left_line = sub_array(output, min_line + 1, #output)
+        alert(left_line)
         vim.fn.append("$", left_line)
       end
     end
     vim.cmd("redraw!")
     vim.cmd('lexpr ""')
     vim.cmd('lwin')
+  else
+    local errors = vim.fn.readfile(error_file)
+    alert(errors)
   end
+  vim.fn.delete(error_file)
 end
 
 return _M
