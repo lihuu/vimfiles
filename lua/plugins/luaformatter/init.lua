7b06038e887cf7c8bcf635c114075c25df54e02d
lihu <1449488533qq@gmail.com>
2022-04-20 18:17:06 +0800

    fix some erros

--- lua/plugins/luaformatter/init.lua
+++ lua/plugins/luaformatter/init.lua
@@ -16,19 +16,19 @@ local _M = {}
 
 function _M.LuaFormat()
   local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-  local config_file = vim.fn.findfile('.lua-format','.;')
-  --local options
-  --if config_file == '' then
-      --options = ""
-  --else
-      --options = " -c " .. config_file
-  --end
+  -- local config_file = vim.fn.findfile('.lua-format','.;')
+  -- local options
+  -- if config_file == '' then
+  -- options = ""
+  -- else
+  -- options = " -c " .. config_file
+  -- end
   local outout_str = vim.fn.system('lua-format -i', lines)
 
   if #outout_str > 0 then
     local output = vim.split(outout_str, "\n")
     local min_line = math.min(#lines, #output)
-    alert(min_line)
+    -- alert(min_line)
     for i = 1, min_line, 1 do
       local output_line = output[i]
       local input_line = lines[i]
@@ -41,7 +41,8 @@ function _M.LuaFormat()
       if min_line == #output then
         vim.fn.deletebufline(vim.fn.bufname('%'), min_line + 1, "$")
       else
-        vim.fn.append("$", sub_array(output, min_line + 1, #output))
+        local left_line = sub_array(output, min_line + 1, #output)
+        vim.fn.append("$", left_line)
       end
     end
     vim.cmd("redraw!")
