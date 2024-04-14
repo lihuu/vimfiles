21816df3dd2c8a38fa6de199e1440668bdb3d620
SilentStorm <1449488533qq@gmail.com>
2024-04-14 17:20:10 +0800

    fix

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -103,11 +103,11 @@ return {
       setup = {
         eslint = function()
           require("lazyvim.util").lsp.on_attach(function(client)
-            if client.name == "eslint" then
-              client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "tsserver" then
-              client.server_capabilities.documentFormattingProvider = false
-            end
+            -- if client.name == "eslint" then
+            --client.server_capabilities.documentFormattingProvider = true
+            -- elseif client.name == "tsserver" then
+            --client.server_capabilities.documentFormattingProvider = false
+            -- end
           end)
         end,
       },
