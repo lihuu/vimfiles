1a1483351acee4ab5009725aba8bd280dc6a4014
SilentStorm <1449488533qq@gmail.com>
2024-09-06 20:40:39 +0800

    feat: more extra configs

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,15 +11,22 @@ local spec = {
   -- add LazyVim and import its plugins
   { "LazyVim/LazyVim", import = "lazyvim.plugins" },
   -- import any extras modules here
+  { import = "lazyvim.plugins.extras.lang.vue" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.git" },
+  { import = "lazyvim.plugins.extras.linting.eslint" },
   { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.tailwind" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
   { import = "lazyvim.plugins.extras.lang.docker" },
-  { import = "lazyvim.plugins.extras.editor.leap" },
   { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.editor.leap" },
+  { import = "lazyvim.plugins.extras.editor.mini-diff" },
+  { import = "lazyvim.plugins.extras.editor.refactoring" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+  { import = "lazyvim.plugins.extras.editor.telescope" },
   { import = "plugins" },
 }
 
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,11 @@
+local config = require("config")
+
+return {
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  {
+    "nvim-tree/nvim-tree.lua",
+    cmd = "NvimTreeToggle",
+    keys = { { "<leader>fe", "<cmd>NvimTreeToggle<cr>", "Toggle NvimTree" } },
+    config = config.nvim_tree,
+  },
+}
