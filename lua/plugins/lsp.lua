2fb8a08bcb3fe7fb3830887e6d07f22876e547c2
SilentStorm <1449488533qq@gmail.com>
2024-09-22 14:40:06 +0800

    feat: config render-markdown plugin

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+        "gopls",
+        "json-lsp",
+        "prettier",
+        "eslint-lsp",
+        "lua-language-server",
+        "typescript-language-server",
+        "vue-language-server",
+        "pyright",
+        "goimports",
+        "gofumpt",
+        "tailwindcss-language-server",
+        "markdown-toc",
+        "markdownlint",
+        "markdownlint-cli2",
+        "marksman",
+        "docker-compose-language-service",
+        "yaml-language-server",
+        "ruff",
+        "ruff-lsp",
+      },
+    },
+  },
+}
