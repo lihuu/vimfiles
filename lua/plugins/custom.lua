f8732b95175e05ebc96ed61f93908983a1521c2d
lihuu <lihu1449488533@outlook.com>
2025-11-07 20:51:18 +0800

    feat: simplify eslint setup function in LSP configuration

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -107,15 +107,7 @@ return {
         eslint = {},
       },
       setup = {
-        eslint = function()
-          require("lazyvim.util").lsp.on_attach(function(client)
-            -- if client.name == "eslint" then
-            --client.server_capabilities.documentFormattingProvider = true
-            -- elseif client.name == "tsserver" then
-            --client.server_capabilities.documentFormattingProvider = false
-            -- end
-          end)
-        end,
+        eslint = function() end,
       },
     },
   },
